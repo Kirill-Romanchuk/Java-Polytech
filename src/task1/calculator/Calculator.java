2627ff7a2da2e7029da716e77c16921efb5393cf
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2025-03-16 01:34:55 +0300

    [TASK-1] add Calculate method for Calculator

--- src/task1/calculator/Calculator.java
+++ src/task1/calculator/Calculator.java
@@ -1,10 +1,60 @@
 package task1.calculator;
 
+import task1.calculator.commands.*;
+import task1.calculator.invoker.Invoker;
+import task1.calculator.parser.ExpressionParser;
+
+import java.util.Stack;
+
 public class Calculator {
     private double result;
+    private String infixExpression;
 
-    public Calculator() {
-        this.result = 0.0f;
+    public Calculator() {}
+
+    public Calculator(String infixExpression) {setExpression(infixExpression);}
+
+    public void setExpression(String infixExpression) { // TODO: What if the expression is wrong and don't worry?
+        result = 0.0f;
+        this.infixExpression = infixExpression;
+    }
+
+    public void Calculate() {
+        ExpressionParser parser = new ExpressionParser();
+        String postfixExpression = parser.infixToPostfix(infixExpression);
+
+        String[] tokens = postfixExpression.split("\\s+");
+        Stack<Double> operandStack = new Stack<>();
+        Invoker invoker = new Invoker();
+
+        for (String token : tokens) {
+            if (Character.isDigit(token.charAt(0))) {
+                double operand = Double.parseDouble(token);
+                operandStack.push(operand);
+                continue;
+            }
+
+            double operand = operandStack.pop();
+
+            switch (token) {
+                case "+":
+                    invoker.putCommand(new AddCommand(this, operand));
+                    break;
+                case "-":
+                    invoker.putCommand(new SubtractCommand(this, operand));
+                    break;
+                case "*":
+                    invoker.putCommand(new MultiplyCommand(this, operand));
+                    break;
+                case "/":
+                    invoker.putCommand(new DivideCommand(this, operand));
+                    break;
+                // Добавьте другие операции, если нужно
+            }
+        }
+
+        invoker.applyCommands();
+
     }
 
     public void add(double value) {
