11aa48d4fe1eb3264bffedd74c088021e753eb96
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2025-03-16 00:18:56 +0300

    [TASK-1] add calculator invoker

---
+++ src/task1/calculator/invoker/Invoker.java
@@ -0,0 +1,19 @@
+package task1.calculator.invoker;
+
+import task1.commands.Command;
+
+import java.util.Stack;
+
+public class Invoker {
+    private Stack<Command> commands = new Stack<Command>();
+
+    public void putCommand(Command command) {
+        commands.add(command);
+    }
+
+    public void applyCommands() {
+        for (Command command: commands) {
+            command.execute();
+        }
+    }
+}
